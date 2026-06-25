855920ba83ef2bf9007e14764c1e7bea4fcd52ff
ls <65608254+yshshr@users.noreply.github.com>
2026-06-25 19:26:30 +0800

    使用剩余参数简化从数组移除

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,17 +1,5 @@
-const removeFromArray = function(arr) {
-  const args = Array.from(arguments);
-  args.forEach(arg => {
-    let done;
-    while(!done){
-      let itemIndex = arr.indexOf(arg);
-      if(itemIndex<0){
-        done = true;
-      }else{
-        arr.splice(itemIndex,1);
-      }
-    }
-  });
-  return arr;
+const removeFromArray = function(arr, ...args) {
+  return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
