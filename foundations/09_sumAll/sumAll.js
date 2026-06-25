8540b69e5c5df8b501b6a19a599d94cabe6f2057
ls <65608254+yshshr@users.noreply.github.com>
2026-06-25 20:46:37 +0800

    优化求整数区间和

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,9 +1,18 @@
 const sumAll = function(num1, num2) {
-  if(num1 <=0 || num2 <=0 || Math.floor(num1)!==num1 || Math.floor(num2) !== num2){
+  if(!Number.isInteger(num1) || !Number.isInteger(num2)){
     return 'ERROR';
   }
-  let xs = num1 > num2 ? (num1 - num2 + 1) : (num2 - num1 + 1);
-  return (num1 + num2) * xs / 2;
+  if(num1 <=0 || num2 <=0){
+    return 'ERROR';
+  }
+  if(num1 > num2){
+    [num1,num2] = [num2,num1];
+  }
+  let sum = 0;
+  for(let i=num1;i<=num2;i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
