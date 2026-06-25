5787452b18b7d14fc450f27e42f922beb8368d73
ls <65608254+yshshr@users.noreply.github.com>
2026-06-25 21:52:27 +0800

    简化保留一位小数

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,13 +1,9 @@
 const convertToCelsius = function(fahrenheit) {
-  let celsius = (fahrenheit - 32) * 5 / 9;
-  celsius = Number(celsius.toFixed(1));
-  return celsius;
+  return Math.round((fahrenheit - 32) * 5 / 9 * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-  let fahrenheit = celsius * 9 / 5 + 32;
-  fahrenheit = Number(fahrenheit.toFixed(1));
-  return fahrenheit;
+  return Math.round((celsius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
