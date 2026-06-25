1778bb63c374eaa04beebaade9598f8f89a1f031
ls <65608254+yshshr@users.noreply.github.com>
2026-06-25 19:49:06 +0800

    整数区间求和

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if(num1 <=0 || num2 <=0 || Math.floor(num1)!==num1 || Math.floor(num2) !== num2){
+    return 'ERROR';
+  }
+  let xs = num1 > num2 ? (num1 - num2 + 1) : (num2 - num1 + 1);
+  return (num1 + num2) * xs / 2;
 };
 
 // Do not edit below this line
