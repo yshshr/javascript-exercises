d47cf53f4329a84cb891b098973536fbab3341bc
ls <65608254+yshshr@users.noreply.github.com>
2026-07-02 16:41:22 +0800

    完成计算器和回文数练习

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // 只保留数字和字母
+  const numberAndCharactor = '1234567890abcdefghijklmnopqrstuvwxyz';
 
+  // 转成小写
+  str = str.toLowerCase();
+
+  // 去除非数字和字母内容
+  const strarr = str.split('').filter(c=>{
+    return numberAndCharactor.includes(c);
+  });
+  const handleStr = strarr.join('');
+
+  // 翻转字符串并比较
+  return handleStr === handleStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
