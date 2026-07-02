0cb0943906e001b23f84f882aa5fc85735f68c95
ls <65608254+yshshr@users.noreply.github.com>
2026-07-02 17:23:14 +0800

    完成斐波那契和最大年龄练习

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if(typeof n !== 'Number'){
+    n = Number(n);
+  }
+  if(n===0){
+    return 0;
+  } else if(n===1){
+    return 1;
+  } else if(n===2){
+    return 1;
+  } else if(n>=3) {
+    return fibonacci(n-1) + fibonacci(n-2);
+  } else if(n<0){
+    return 'OOPS';
+  }
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest,cur)=>{
+    const curYear = (new Date).getFullYear();
+    const oldestLiveYear = (oldest.yearOfDeath || curYear)- oldest.yearOfBirth;
+    const curLiveYear = (cur.yearOfDeath || curYear) - cur.yearOfBirth;
+    if(oldestLiveYear < curLiveYear){
+      oldest = cur;
+    }
+    return oldest;
+  })
 };
 
 // Do not edit below this line
