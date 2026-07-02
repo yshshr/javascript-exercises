0f4f134f7a4fd7cba256c69b026fdebf61c650f8
ls <65608254+yshshr@users.noreply.github.com>
2026-07-02 14:44:22 +0800

    完成计算器

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numarr) {
+	return numarr.reduce((sum,cur)=>sum+cur,0);
 };
 
-const multiply = function() {
-
+const multiply = function(numarr) {
+  return numarr.reduce((mul,cur)=>mul * cur,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let factorial = 1;
+	for(let i=n;i>0;i--){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
